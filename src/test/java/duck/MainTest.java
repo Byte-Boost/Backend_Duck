2531c8cbf0adedd462a18a7345d2d8da9c9bc46b
Vinicius Forcato <76211125+nininhosam@users.noreply.github.com>
2023-09-21 10:17:04 -0300

    Rearranged Backend

---
+++ src/test/java/duck/MainTest.java
@@ -0,0 +1,12 @@
+
+package duck;
+
+import net.byteboost.duck.Main;
+import org.junit.jupiter.api.Test;
+class MainTest {
+
+    @Test void appHasAGreeting() {
+        Main classUnderTest = new Main();
+
+    }
+}
