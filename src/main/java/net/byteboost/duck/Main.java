c2efc2ae2b6570029cc9fbb2ac913a1071d07fc0
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2023-10-03 17:19:23 -0300

    [style]: add icon and locked size of app screen

--- src/main/java/net/byteboost/duck/Main.java
+++ src/main/java/net/byteboost/duck/Main.java
@@ -5,24 +5,27 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Objects;
 
-public class Main extends Application {
+public class Main extends Application{
     public static void main(String[] args) {
         launch(args);
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        primaryStage.getIcons().add(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("/images/ducklogoround.png"))));
+
+        final boolean resizable = primaryStage.isResizable();
+
         Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("/fxml/login.fxml")));
-
         primaryStage.setTitle("Login");
+        primaryStage.setResizable(!resizable);
         primaryStage.setScene(new Scene(root));
-
         primaryStage.show();
-
-
     }
+
 }
