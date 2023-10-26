3d2e8ae27ffeb9e782221c892ae500a12ac0c4f9
Markos Nunes <105757405+MarkVN2@users.noreply.github.com>
2023-10-26 17:00:58 -0300

    [refactor]: eye-opened to eye-open

--- src/main/java/net/byteboost/duck/gui/LoginController.java
+++ src/main/java/net/byteboost/duck/gui/LoginController.java
@@ -1,10 +1,8 @@
 package net.byteboost.duck.gui;
 
-import com.sun.javafx.scene.control.behavior.ToggleButtonBehavior;
 import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.event.WeakEventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -19,9 +17,6 @@ import net.byteboost.duck.models.User;
 import net.byteboost.duck.utils.DBUtils;
 import net.byteboost.duck.utils.GUIUtils;
 
-import javax.swing.*;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -66,7 +61,7 @@ public class LoginController implements Initializable {
                 if (btn_password.isSelected()) {
 
                     InputStream stream = null;
-                    stream = getClass().getResourceAsStream("/images/eye-opened.png");
+                    stream = getClass().getResourceAsStream("/images/eye-open.png");
                     assert stream != null;
                     Image image = new Image(stream);
 
