51cefefb1d7b580dce973f70f6934060123d23ee
Vinicius Forcato <76211125+nininhosam@users.noreply.github.com>
2023-11-15 20:33:25 -0300

    [feat]: Password Encryption
    No more plaintext passwords recorded on the database.

---
+++ src/main/java/net/byteboost/duck/utils/PasswordEncryptionUtils.java
@@ -0,0 +1,28 @@
+package net.byteboost.duck.utils;
+
+import org.apache.pdfbox.tools.Encrypt;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+public class PasswordEncryptionUtils {
+
+    public static String getSalt() throws Exception {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[32];
+        random.nextBytes(salt);
+        return Base64.getEncoder().encodeToString(salt);
+    }
+    public static String getEncryptedPassword(String password, String salt) throws Exception {
+        byte[] decSalt = Base64.getDecoder().decode(salt);
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), decSalt, 10012, 128);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        SecretKey key = factory.generateSecret(spec);
+        byte[] EncryptedPass = key.getEncoded();
+        return Base64.getEncoder().encodeToString(EncryptedPass);
+    }
+}
